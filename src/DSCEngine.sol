f1621b465fb179054705267c4a2137077ba1a7b8
farazmb <af602699@gmail.com>
2024-10-08 15:43:26 +0500

    updated files

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -23,6 +23,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+
 /*
  * @title DSCEngine
  * @author Faraz 
@@ -37,15 +39,83 @@ pragma solidity 0.8.27;
  *
  * Our DSC system should always be "overcollateralized". At no point, should the value of
  * all collateral < the $ backed value of all the DSC.
- *
+ * Our DSC system should always be overcollateralized. At no point should the value shouold the 
+   value of all collateral  <= the $ backed value of the DSC.
  * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
  * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
  * @notice This contract is based on the MakerDAO DSS system
  */
-contract DSCEngine{
+contract DSCEngine {
+    //------------Errors-------------------------
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressAndPriceFeedAddressMustBeSame();
+    error DSCEngine__NotAllowedToken();
 
-    function depositCollateralAndMintDSC() external{}
-    function redeemCollateralForDSC() external{}
+    //------------state variables----------------
+
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    DecentralizedStableCoin private immutable i_dsc;
+
+    //------------modifiers----------------------
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    //------------funtions--------------------
+
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        //USD Price Feeds
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressAndPriceFeedAddressMustBeSame();
+        }
+
+        //e.g. ETH/USD,  BTC/USD,  MKR/USD, etc
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    //------------External functions-----------
+
+    function depositCollateralAndMintDSC() external {}
+
+    // @param tokenCollateralAddress the address of the token to deposit as collateral
+    //@param amountCollateral the amount of collateral to be deposit
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    )
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+    {}
+
+    function redeemCollateralForDSC() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDSC() external {}
+
     function burnDSC() external {}
 
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
 }
